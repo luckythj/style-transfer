4a9e6b861f837ea67d672da468f4561d99d661f1
hongjie.tan <luckythj@163.com>
2018-05-12 14:20:05 +0800

    add readme

---
+++ data/download.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+wget "http://cs231n.stanford.edu/squeezenet_tf.zip"
+unzip squeezenet_tf.zip
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+unzip train2014.zip
+
--- sample_transform.sh
+++
@@ -1,5 +0,0 @@
-python fast_style_transfer.py \
-  --checkpoint 'test.ckpt'\
-  --input_image 'data/test.jpg'\
-  --output_path 'data/test_out.jpg' \
-  --image_size 256
